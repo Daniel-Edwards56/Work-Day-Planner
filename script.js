c55e5297264915f908fd5e0df664c16b0041d076
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2020-12-21 22:06:32 -0500

    wrote readme, cleaned up js, added extra descriptive comments

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ var renderTimeBlocks = () => {
         rowEl = $("<div>").attr("class","row");
         hourEl = $("<div>" + time[i] + "</div>").attr("class", "col-lg-1 hour");
         col = $("<div>").attr("class", "col-lg-10");
-        textAreaEl =$("<textarea>").attr("value", "").attr("class", "description col-lg-12").attr("id",time[i]);
+        textAreaEl =$("<textarea>").attr("value", "").attr("class", "description col-lg-10").attr("id",time[i]);
         index.push(textAreaEl.attr("id"));
-        saveEl = $("<div>").attr("class", "col-lg-1");
-        saveBtnEl = $("<button>").attr("class", "saveBtn").attr("id", time[i]).attr("type", "submit").text("Save")
+        // saveEl = $("<div>").attr("class", "col-lg-1");
+        saveBtnEl = $("<button>").attr("class", "saveBtn col-lg-1").attr("id", time[i]).attr("type", "submit").text("Save")
         //Appending time blocks to html
         col.append(textAreaEl)
-        saveEl.append(saveBtnEl)
+        // saveEl.append(saveBtnEl)
         rowEl.append(hourEl)
         rowEl.append(col)
-        rowEl.append(saveEl)
+        rowEl.append(saveBtnEl)
         holdingContainer.append(rowEl)
 
     }
@@ -42,6 +42,7 @@ var timeOfDay = () => {
         formattedTime.push((moment().startOf("day").add(timePoint, "hours").format("HH:mm A")))
     }
 }
+//set time/color for each text area according to current time
 var currentTime = () => {
 currentHour = moment().format("HH:mm A")
 var timeStamp = $("textarea").get()
@@ -66,6 +67,14 @@ timeStamp.forEach(timeSlot => {
     }
     
 });
+}
+//save buttons saves to local storage
+function saveSchedule(event){
+event.preventDefault()
+var input = $(this).parent().parent().find(".description", ['textarea'])[0].value
+    var time = $(this).parent().parent().find(".description", ['textarea'])[0].id;
+    localStorage.setItem(time, input)
+
 }
 timeOfDay()
 renderTimeBlocks()
